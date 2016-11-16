f3e4858c0a9bde77eefb041a4c2b5c68c7e928cb
A.S.MMehediHasan <asmmehedihasan09@gmail.com>
2016-11-17 04:45:31 +0600

    Admin Site, Router Added, Email Template Added

--- router/index.js
+++ router/index.js
@@ -10,3 +10,33 @@ FlowRouter.route('/signup', {
     BlazeLayout.render('BasicLayout', {content: 'signup'});
   }
 });
+
+FlowRouter.route('/dashboard', {
+  action: function (params, queryParams) {
+    BlazeLayout.render('DashboardLayout', {content: 'DashboardHome'});
+  }
+});
+
+FlowRouter.route('/dashboard/email/inbox', {
+  action: function (params, queryParams) {
+    BlazeLayout.render('DashboardLayout', {content: 'EmailInbox'});
+  }
+});
+
+FlowRouter.route('/dashboard/email/draft', {
+  action: function (params, queryParams) {
+    BlazeLayout.render('DashboardLayout', {content: 'EmailDraft'});
+  }
+});
+
+FlowRouter.route('/dashboard/email/sent', {
+  action: function (params, queryParams) {
+    BlazeLayout.render('DashboardLayout', {content: 'EmailSent'});
+  }
+});
+
+FlowRouter.route('/dashboard/email/trash', {
+  action: function (params, queryParams) {
+    BlazeLayout.render('DashboardLayout', {content: 'EmailTrash'});
+  }
+});
